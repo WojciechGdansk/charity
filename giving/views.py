eb1658b3ab0d71dcadfa5ed59de2da2bf9b9508a
Wojciech Plokarz <wojciech.plokarz@op.pl>
2023-03-15 13:12:27 +0100

    counting donated bags and supported institutions

--- giving/views.py
+++ giving/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.views import View
-
+from giving.models import Donation, Insitution
+from django.db.models import Sum
 
 # Create your views here.
 class LandingPare(View):
     def get(self, request):
-        return render(request, 'index.html')
+        supported_institution = set()
+        all_insitutions = Insitution.objects.all()
+        for institution in all_insitutions:
+            supported_institution.add(institution.name)
+        return render(request, 'index.html', context={
+            "bags": Donation.objects.aggregate(Sum('quantity'))['quantity__sum'],
+            'supported_institutions': len(supported_institution)
+        })
 
 
 class AddDonation(View):
