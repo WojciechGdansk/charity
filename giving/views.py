d52125764a3470074f063448dcc281bc735aaede
Wojciech Plokarz <wojciech.plokarz@op.pl>
2023-03-16 12:22:37 +0100

    #3 registration form works

--- giving/views.py
+++ giving/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect, reverse
 from django.views import View
 from giving.models import Donation, Insitution
 from django.db.models import Sum
 from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+from django.core.validators import validate_email
+
 
 # Create your views here.
 class LandingPage(View):
@@ -12,11 +16,11 @@ class LandingPage(View):
         for institution in all_insitutions:
             supported_institution.add(institution.name)
         foundations = Insitution.objects.filter(type=1).order_by('id')
-        paginator_foundations = Paginator(foundations,5)
+        paginator_foundations = Paginator(foundations, 5)
         page = request.GET.get('page')
         foundations = paginator_foundations.get_page(page)
         non_governmental_organization = Insitution.objects.filter(type=2).order_by('id')
-        paginator_nongover = Paginator(non_governmental_organization,5)
+        paginator_nongover = Paginator(non_governmental_organization, 5)
         non_governmental_organization = paginator_nongover.get_page(page)
         local_collection = Insitution.objects.filter(type=3).order_by('id')
         paginator_local_collections = Paginator(local_collection, 5)
@@ -43,3 +47,32 @@ class Login(View):
 class Register(View):
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        first_name = request.POST.get('name')
+        last_name = request.POST.get('surname')
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        error_msg = ''
+
+        if password != password2:
+            error_msg = "Podane hasła nie są identyczne\n"
+
+        if User.objects.filter(username=username).exists():
+            error_msg += "Podany użytkownik już istnieje\n"
+        try:
+            validate_email(username)
+        except ValidationError:
+            error_msg += "Niepoprawny adres email"
+
+        if error_msg:
+            return render(request, "register.html", {'error_msg': error_msg})
+
+        User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password
+        )
+        return redirect(reverse("login"))
