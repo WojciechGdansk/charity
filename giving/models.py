981e83ef9924140cea88dc73dd8aa5ddd7796a8d
Wojciech Plokarz <wojciech.plokarz@op.pl>
2023-03-20 21:09:47 +0100

    better display of Fundchoices

--- giving/models.py
+++ giving/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class FundChoices(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
